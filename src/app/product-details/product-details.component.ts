afd3f3f14a5643a9f37d6de9835d91cdb9bba7d2
thinhnnd <thinhnie.nguyen@gmail.com>
2019-10-28 16:59:22 +0700

    managing data -Use the cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../products';
-
+import { products } from '../producst';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -10,8 +10,15 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor( private route: ActivatedRoute
+  constructor( 
+    private route: ActivatedRoute,
+    private cartService: CartService
    ) { }
+
+  addToCard(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
