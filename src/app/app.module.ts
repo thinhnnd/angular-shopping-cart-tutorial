aab91606e477b6bc0b60e405b1e0a096fad0d9fd
thinhnnd <thinhnie.nguyen@gmail.com>
2019-10-28 20:08:55 +0700

    Managing Data - Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -16,10 +17,9 @@ import { CartService } from './cart.service';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      {
-        path: 'products/:productId',
-        component: ProductDetailsComponent
-      }
+      { path: 'products/:productId', component: ProductDetailsComponent
+      },
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
@@ -28,6 +28,7 @@ import { CartService } from './cart.service';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../producst';
+import { products } from '../products';
 import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
@@ -15,7 +15,7 @@ export class ProductDetailsComponent implements OnInit {
     private cartService: CartService
    ) { }
 
-  addToCard(product) {
+  addToCart(product) {
     window.alert('Your product has been added to the cart!');
     this.cartService.addToCart(product);
   }
